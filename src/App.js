7090e851efb741de46b777a89ab59ffa067b09ce
KristinaKolesnyk <kkristina.kolesnyk@gmail.com>
2025-09-26 19:04:07 -0400

    added api

--- src/App.js
+++ src/App.js
@@ -1,24 +1,159 @@
 import React, {Component} from "react";
 import './App.css';
 import 'tachyons';
+import Clarifai from 'clarifai';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import ParticlesBg from 'particles-bg'
 
+const MODEL_ID = 'face-detection';
+
+/*
+const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = '124acf4df6e54d249d72d207188253e1';
+    const USER_ID = 'clarifai';
+    const APP_ID = 'main';
+    const IMAGE_URL = imageUrl;
+
+
+        const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                    }
+                }
+            }
+        ]
+    });
+
+
+    return {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+}
+*/
+
+/////////delete
+const returnClarifaiRequestOptions = (imageUrl) => {
+    const PAT = '124acf4df6e54d249d72d207188253e1';
+    const USER_ID = 'clarifai';
+    const APP_ID = 'main';
+    const IMAGE_URL = imageUrl;
+
+    const raw = JSON.stringify({
+        "user_app_id": {
+            "user_id": USER_ID,
+            "app_id": APP_ID
+        },
+        "inputs": [
+            {
+                "data": {
+                    "image": {
+                        "url": IMAGE_URL
+                        // "base64": IMAGE_BYTES_STRING
+                    }
+                }
+            }
+        ]
+    });
+
+    const requestOptions = {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Authorization': 'Key ' + PAT
+        },
+        body: raw
+    };
+
+    return requestOptions
+}
+
+
+
+
+////////
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {}
+        }
+    }
+
+    displayFaceBox = (box) => {
+        this.setState({box: box})
+    }
+
+    calculateFaceLocation = (data) => {
+        const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+        const image = document.getElementById('inputimage');
+        const width = Number(image.width);
+        const height = Number(image.height);
+        return {
+            leftCol: clarifaiFace.left_col * width,
+            topRow: clarifaiFace.top_row * height,
+            rightCol: width - (clarifaiFace.right_col * width),
+            bottomRow: height - (clarifaiFace.bottom_row * height)
+        }
+    }
+
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    }
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+
+        fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(this.state.input))
+            .then(response => response.json())
+            .then(response => {
+                console.log('hi', response)
+                if (response) {
+                    fetch('http://localhost:3000/image', {
+                        method: 'put',
+                        headers: {'Content-Type': 'application/json'},
+                        body: JSON.stringify({
+                            id: this.state.user.id
+                        })
+                    })
+                        .then(response => response.json())
+                        .then(count => {
+                            this.setState(Object.assign(this.state.user, {entries: count}))
+                        })
+                }
+                this.displayFaceBox(this.calculateFaceLocation(response))
+            })
+            .catch(err => console.log(err));
+    }
+
     render() {
         return (
             <div className="App">
-                <ParticlesBg color="#ffffff" num={60} type="cobweb" bg={true} />
+                <ParticlesBg color="#ffffff" num={60} type="cobweb" bg={true}/>
                 <Navigation/>
                 <Logo/>
                 <Rank/>
-                <ImageLinkForm/>
-                {/*
-                    <FaceRecognition/>*/}
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
             </div>
         )
     }
